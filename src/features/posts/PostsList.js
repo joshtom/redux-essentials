f258316658d972c20745e1cbe99b6f90ba4f97ef
joshtom <olajidejoshua4real@gmail.com>
2020-07-23 20:11:57 +0100

    Added postUpdated reducer to update existing post

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 // Component to list all the post
 import React from 'react'
 import { useSelector } from 'react-redux'; // React can use this selector to read data from the redux store
+import { Link } from 'react-router-dom';
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts);
@@ -9,6 +10,9 @@ export const PostsList = () => {
         <article className="post-excerpt" key={post.id}>
             <h3> {post.title} </h3>
             <p> {post.content.substring(0, 100)} </p>
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                View Post
+            </Link>
         </article>
     ))
 
