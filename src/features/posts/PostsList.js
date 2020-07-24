7b26769c00494855bf8e5e29ea550d80d47af733
joshtom <olajidejoshua4real@gmail.com>
2020-07-24 19:50:52 +0100

    Update SinglePost component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -8,7 +8,7 @@ export const PostsList = () => {
     const posts = useSelector(state => state.posts);
 
     // Sort posts in reverse chronological order by datetime string
-    // const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
     const renderedPosts = posts.map(post => (
         <article className="post-excerpt" key={post.id}>
@@ -17,7 +17,6 @@ export const PostsList = () => {
             <Link to={`/posts/${post.id}`} className="button muted-button">
                 View Post
             </Link>
-            <PostAuthor userId={post.user} />
         </article>
     ))
 
