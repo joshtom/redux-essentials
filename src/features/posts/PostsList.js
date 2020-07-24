91dcf2e27bd6d78d4902a6b78b7526b112fe74da
joshtom <olajidejoshua4real@gmail.com>
2020-07-24 20:08:02 +0100

    Postlist component updated

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'; // React can use this selector to read data from the redux store
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts);
@@ -10,15 +11,21 @@ export const PostsList = () => {
     // Sort posts in reverse chronological order by datetime string
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = posts.map(post => (
-        <article className="post-excerpt" key={post.id}>
-            <h3> {post.title} </h3>
-            <p> {post.content.substring(0, 100)} </p>
-            <Link to={`/posts/${post.id}`} className="button muted-button">
-                View Post
-            </Link>
-        </article>
-    ))
+const renderedPosts = orderedPosts.map(post => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+})
 
 
     return (
